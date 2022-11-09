2b106fe1e10bbf27baf29198f360952191dc54bf
mdshafikulislam <shafikul1170@gmail.com>
2022-11-09 23:33:58 +0600

    Even or odd

--- RandomProblem/Decision & Looping/Even or Odd Number.cpp
+++ RandomProblem/Decision & Looping/Even or Odd Number.cpp
@@ -1,8 +1,20 @@
-#include<iostream>
+// even or odd number
+#include <iostream>
 
 using namespace std;
 
-int main(){
-    cout<<"sk";
+int main()
+{
+
+    int number;
+    cout << "Input the Number : ";
+    cin >> number;
+
+    if (number % 2 == 0) cout << "Even Number" << endl;
+    else cout << "Odd Number" << endl;
+
+    //or Ternary Operator in C++
+    string result = (number % 2 == 0) ? "Even" : "Odd";
+    cout << result;
     return 0;
 }
