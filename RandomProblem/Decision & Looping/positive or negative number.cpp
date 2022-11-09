72f390d72da413e36939abb4d5b71b72a11b3d63
mdshafikulislam <shafikul1170@gmail.com>
2022-11-09 23:44:08 +0600

    Check if a Number is Positive or Negative

---
+++ RandomProblem/Decision & Looping/positive or negative number.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int number;
+    cout << "Input the Number : ";
+    cin >> number;
+
+    if (number > 0) cout << "Positive Number" << endl;
+    else if (number < 0) cout << "Negative Number" << endl;
+    else cout << "Zero";
+
+    //or Ternary Operator in C++
+    string result = (number>0) ? "Positive" : "Negative";
+    cout << result;
+    return 0;
+}
