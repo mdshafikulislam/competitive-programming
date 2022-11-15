964238b7dbaa37be43880b9de41b60c39d1fe608
mdshafikulislam <shafikul1170@gmail.com>
2022-11-15 17:11:14 +0600

    Search an Element in an array

--- RandomProblem/Array-related program/Search element in array.cpp
+++ RandomProblem/Array-related program/Search element in array.cpp
@@ -1,8 +1,32 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
-int main(){
-    cout<<"sk";
+int main()
+{
+    int arr[100], i, n, x;
+
+    cout << "Enter the Array Limit :";
+    cin >> n;
+    for (i = 0; i < n; i++)
+    {
+        cout << "Enter The Value :";
+        cin >> arr[i];
+    }
+
+    cout << "Enter the Search Element:";
+    cin >> x;
+    for (i = 0; i < n; i++)
+    {
+        if (arr[i] == x)
+        {
+            cout << "The Index Position :" << i << " "
+                 << "Search Value :" << arr[i];
+            return 0;
+        }
+        else
+            cout << "Not found";
+    }
+
     return 0;
 }
