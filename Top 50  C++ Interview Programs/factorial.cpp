c893e9250006ef99d6ea8cee2886b9b35ca5f810
mdshafikulislam <shafikul1170@gmail.com>
2022-11-16 20:33:36 +0600

    Factoria

--- Top 50  C++ Interview Programs/factorial.cpp
+++ Top 50  C++ Interview Programs/factorial.cpp
@@ -1,8 +1,22 @@
-#include<iostream>
-
+#include <iostream>
 using namespace std;
 
-int main(){
-    cout<<"sk";
+int main() {
+    int n;
+    long factorial = 1.0;
+
+    cout << "Enter a positive integer: ";
+    cin >> n;
+
+    if (n < 0)
+        cout << "Error! Factorial of a negative number doesn't exist.";
+    else {
+        for(int i = 1; i <= n; ++i) {
+            cout<<i<<"Loop :"<<factorial<<endl;
+            factorial *= i;
+        }
+        cout << "Factorial of " << n << " = " << factorial;
+    }
+
     return 0;
 }
