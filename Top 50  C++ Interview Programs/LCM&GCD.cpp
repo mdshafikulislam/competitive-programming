1b9d1092084e77c632aba5c905cc0c05be9c2308
mdshafikulislam <shafikul1170@gmail.com>
2023-01-26 17:30:34 +0600

    GCD & LCM

--- Top 50  C++ Interview Programs/LCM&GCD.cpp
+++ Top 50  C++ Interview Programs/LCM&GCD.cpp
@@ -3,6 +3,28 @@
 using namespace std;
 
 int main(){
-    cout<<"sk";
+    int number1,number2,gcd,lcm,remainder;
+
+    cout<<"1st input Number:";
+    cin>>number1;
+
+    cout<<"2nd input Number:";
+    cin>>number2;
+
+    int num1=number1;
+    int num2=number2;
+
+    while(num2 != 0){
+        remainder=num1%num2;
+        num1=num2;
+        num2=remainder;
+    }
+    gcd=num1;
+    lcm=(number1*number2)/gcd;
+
+    cout<<"Greatest common divisor of : "<<gcd<<endl;
+    cout<<"Greatest common divisor of : "<<lcm;
+
+
     return 0;
 }
