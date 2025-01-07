0afbde41dd89be621138448e604c15aa75cc71d5
shafikul7 <shafikul1170@gmail.com>
2025-01-07 19:07:29 +0600

    update project task management

--- Reactjs project/React-Powered Task Management/src/App.jsx
+++ Reactjs project/React-Powered Task Management/src/App.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import Header from "./Header";
 import HeroSection from "./HeroSection";
 import Footer from "./Footer";
+import TaskBoard from "./Task/TaskBoard";
 
 const App = () => {
   return (
     <div>
       <Header></Header>
+      <div className="flex flex-col justify-end items-center">
       <HeroSection></HeroSection>
+      <TaskBoard></TaskBoard>
+      </div>
       <Footer></Footer>
     </div>
   );
---
+++ Reactjs project/React-Powered Task Management/src/Task/TaskBoard.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import SearchTask from "./SearchTask";
+import TaskAction from "./TaskAction";
+import TaskList from "./TaskList";
+import { useState } from "react";
+
+const TaskBoard = () => {
+    const defaultTask = {
+      id: crypto.randomUUID(),
+      title: "Task 1",
+      description: "This is a sample task",
+      tags: ["web", "React", "js"],
+      priority: "High",
+      isFavorite: false,
+    };
+  const [tasks, setTask] = useState([defaultTask]);
+
+
+  return (
+    <>
+      <section className="mb-20" id="tasks">
+        <div className="container">
+          <div className="p-2 flex justify-end">
+            <SearchTask></SearchTask>
+          </div>
+
+          <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
+            <TaskAction></TaskAction>
+            <TaskList tasks={tasks}></TaskList>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default TaskBoard;
