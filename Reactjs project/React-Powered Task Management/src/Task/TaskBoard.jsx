1e43fd6af9d7b8edd83a3866d96ec2e659af56bd
shafikul7 <shafikul1170@gmail.com>
2025-01-08 17:48:23 +0600

    update project task management

--- Reactjs project/React-Powered Task Management/src/Task/TaskBoard.jsx
+++ Reactjs project/React-Powered Task Management/src/Task/TaskBoard.jsx
@@ -3,29 +3,35 @@ import SearchTask from "./SearchTask";
 import TaskAction from "./TaskAction";
 import TaskList from "./TaskList";
 import { useState } from "react";
+import AddTaskModal from "./AddTaskModal";
 
 const TaskBoard = () => {
-    const defaultTask = {
-      id: crypto.randomUUID(),
-      title: "Task 1",
-      description: "This is a sample task",
-      tags: ["web", "React", "js"],
-      priority: "High",
-      isFavorite: false,
-    };
+  const defaultTask = {
+    id: crypto.randomUUID(),
+    title: "Task 1",
+    description: "This is a sample task",
+    tags: ["web", "React", "js"],
+    priority: "High",
+    isFavorite: false,
+  };
   const [tasks, setTask] = useState([defaultTask]);
-
+  const [showAddModal,setShowAddModal]=useState(false);
+  const handelAddTask = (newTask) => {
+   setTask(...tasks,newTask);
+   setShowAddModal(false);
+  };
 
   return (
     <>
       <section className="mb-20" id="tasks">
+       {showAddModal &&  <AddTaskModal onSave={handelAddTask}/> }
         <div className="container">
           <div className="p-2 flex justify-end">
             <SearchTask></SearchTask>
           </div>
 
           <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
-            <TaskAction></TaskAction>
+            <TaskAction onAddClick={()=>setShowAddModal(true)}></TaskAction>
             <TaskList tasks={tasks}></TaskList>
           </div>
         </div>
